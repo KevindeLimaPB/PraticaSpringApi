ac42b439c976f8a3a9a0a8b3e69b09565d77edf4
Kevin De Lima Alves <kevindelimaalves06@gmail.com>
2026-09-26 12:05:31 -0300

    Controllers VIP feito

--- src/main/java/com/sistema/mercadoOnline/Controller/VIPController.java
+++ src/main/java/com/sistema/mercadoOnline/Controller/VIPController.java
@@ -1,9 +1,32 @@
 package com.sistema.mercadoOnline.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sistema.mercadoOnline.Database.Model.ClienteVipEntity;
+import com.sistema.mercadoOnline.Dto.ClienteVIPDTO;
+import com.sistema.mercadoOnline.Service.VIPService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/vip")
+@RequiredArgsConstructor
+@Validated
 public class VIPController {
+
+    private final VIPService vipService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ClienteVipEntity> findAll(){
+        return vipService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ClienteVipEntity save(@RequestBody @Valid ClienteVIPDTO clienteVIPDTO) throws Exception{
+        return vipService.save(clienteVIPDTO);
+    }
 }
